f915fc700bbcab1789cf01d011af939e1484bb58
Артём Иванов <50824691+arty63@users.noreply.github.com>
2022-08-11 00:05:23 +0400

    add files

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Header, Footer, Main} from './components/view/layout'
+
+function App() {
+  
+  return (
+    <div>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
+     
+  );
+};
+
+export default App;
